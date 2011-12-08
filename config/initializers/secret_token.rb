4776f3e39402fc836b0d13a8186ebc41745f3f91
Narihiro Nakamura <authornari@gmail.com>
2011-12-08 11:33:24 +0900

    init rails 3.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WikiWikiGem::Application.config.secret_token = 'bb0fb6a6abf1144c7d54c2aeb34e709006c926d293647ed809b0cf3da8c1b39a92bc4de75d10f2339ebb16c8c4b9651ebf192d546e66641953ce869f850087dc'
